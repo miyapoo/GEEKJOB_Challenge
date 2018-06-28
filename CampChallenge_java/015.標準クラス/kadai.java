18ad399a4a792917894de1dc3c53a0f3744bcd16
miyuki miyake <miyamiyupoo@gmail.com>
2018-06-28 14:56:20 +0900

    java/標準クラス課題提出します

---
+++ CampChallenge_java/015.標準クラス/kadai.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+/**
+ *
+ * @author miyuki
+ */
+public class kadai {
+    public static void main(String[]args){
+        String name="三宅未有紀";
+        
+        System.out.println("文字数："+name.length());
+        System.out.println("バイト数："+name.getBytes().length);
+    }
+    
+}
